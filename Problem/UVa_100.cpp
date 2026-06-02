c597e4ed329fc5f0f4d8bfb3fd1f45e69ffb87de
A1145515 <a1145515@mail.nuk.edu.tw>
2026-06-02 13:32:05 +0000

    update

---
+++ Problem/UVa_100.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+long long cycle(long long n){
+    long long cnt = 1;
+    while(n != 1){
+        if(n % 2)
+            n = 3 * n + 1;
+        else
+            n /= 2;
+        cnt++;
+    }
+    return cnt;
+}
+
+int main(){
+    int i, j;
+    while(scanf("%d %d", &i, &j) == 2){
+        int a = i;
+        int b = j;
+        if(a > b){
+            int t = a;
+            a = b;
+            b = t;
+        }
+        long long ans = 0;
+        for(int k = a; k <= b; k++){
+            long long len = cycle(k);
+            if(len > ans)
+                ans = len;
+        }
+        printf("%d %d %lld\n", i, j, ans);
+    }
+    return 0;
+}
