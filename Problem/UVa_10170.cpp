7e940b8e86489a042a596b1d8a9535ce4d7ce4a2
A1145515 <a1145515@mail.nuk.edu.tw>
2026-06-07 10:16:22 +0000

    1-2

--- Problem/UVa_10170.cpp
+++ Problem/UVa_10170.cpp
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
+long long S, D;
+
+long long calc(long long k) {
+    return k * S + k * (k - 1) / 2;
+}
+
 int main() {
-    long long S, D;
 
     while (scanf("%lld %lld", &S, &D) == 2) {
 
-        long long n = D - S + 1;
-        long long ans = n * (S + D) / 2;
+        long long l = 1, r = 2000000000, ans = 1;
 
-        printf("%lld\n", ans);
+        while (l <= r) {
+
+            long long mid = (l + r) / 2;
+
+            if (calc(mid) >= D) {
+                ans = mid;
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+
+        printf("%lld\n", S + (ans - 1));
     }
 }
