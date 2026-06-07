7a2b4425b12449d00406805ee8cb733147f7cf06
A1145515 <a1145515@mail.nuk.edu.tw>
2026-06-07 10:32:47 +0000

    1-4

--- Problem/UVa_10409.cpp
+++ Problem/UVa_10409.cpp
@@ -2,56 +2,58 @@
 #include <string.h>
 
 int main() {
-    int n;
 
-    while (scanf("%d", &n) == 1) {
-        if (n == 0) {
-            break;
-        }
+    char cmd[20];
+
+    while (1) {
+
+        int n;
+        scanf("%d", &n);
+
+        if (n == 0) break;
 
         int top = 1, bottom = 6;
         int north = 2, south = 5;
-        int east = 3, west = 4;
+        int west = 3, east = 4;
 
-        char cmd[20];
+        while (n--) {
 
-        for (int i = 0; i < n; i++) {
             scanf("%s", cmd);
 
-            int tmp;
-
             if (strcmp(cmd, "north") == 0) {
-                tmp = top;
+
+                int tmp = top;
                 top = south;
                 south = bottom;
                 bottom = north;
                 north = tmp;
             }
-
             else if (strcmp(cmd, "south") == 0) {
-                tmp = top;
+
+                int tmp = top;
                 top = north;
                 north = bottom;
                 bottom = south;
                 south = tmp;
             }
-
             else if (strcmp(cmd, "east") == 0) {
-                tmp = top;
+
+                int tmp = top;
                 top = west;
                 west = bottom;
                 bottom = east;
                 east = tmp;
             }
-
             else if (strcmp(cmd, "west") == 0) {
-                tmp = top;
+
+                int tmp = top;
                 top = east;
                 east = bottom;
                 bottom = west;
                 west = tmp;
             }
         }
+
         printf("%d\n", top);
     }
 }
